8645c06c94df2b370d868817a65f1065331ff607
mskian <10300271+mskian@users.noreply.github.com>
2023-03-26 13:18:05 +0530

    Update workflow for more OS test

---
+++ os.py
@@ -0,0 +1,17 @@
+import os
+
+HOME = os.path.expanduser('~')
+APP_NAME = 'python-cricket-score'
+CONFIG_NAME = 'score'
+GENERATE_APP = os.path.join(HOME, APP_NAME)
+READ_DATA = os.path.join(HOME, APP_NAME, CONFIG_NAME + '.yaml')
+
+if not os.path.exists(GENERATE_APP):
+         os.mkdir(GENERATE_APP)
+         with open(os.path.join(GENERATE_APP, 'score.yaml'), 'w') as f:
+            f.write("url: '<Update Current Live Match URL From Cricbuzz>'")
+         print("App Directory ", APP_NAME, " Created ")
+         print(READ_DATA)
+else:
+        print("Directory ", APP_NAME, " already exists")
+        print(READ_DATA)
