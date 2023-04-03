8f76e0df419c4678201b5d6f56435dbadaa93eac
mskian <10300271+mskian@users.noreply.github.com>
2023-04-03 18:56:09 +0530

    ntfy: fix OS environ Error

--- ntfy/ntfy.py
+++ ntfy/ntfy.py
@@ -5,12 +5,12 @@ import requests
 
 load_dotenv()
 
-URL = os.get('MATCHURL')
+URL = os.environ['MATCHURL']
 READ = requests.get(URL)
 DATA = json.loads(READ.content.decode())
 title = DATA['title']
 
-url = os.getenv("URL")
+url = os.environ["URL"]
 message = "\n\n" + DATA['title'] + "\n\n" + "➡ Current Score: " + DATA['livescore'] + "\n\n" + "➡ Run Rate: " + DATA['runrate'] + "\n\n" + "➡ Current Batsman: " + DATA['batterone'] + "Runs:" + DATA['batsmanonerun'] + " Balls: " + DATA['batsmanoneball'] + " - SR: " + DATA['batsmanonesr'] + "\n\n" + "➡ Other Batsman: " + DATA['battertwo'] + " Runs: " + DATA['batsmantworun'] + " Balls: " + \
     DATA['batsmantwoball'] + "- SR: " + DATA['batsmantwosr'] + "\n\n" + "➡ Current Bowler: " + DATA['bowlerone'] + " - Overs: " + DATA['bowleroneover'] + " run: " + DATA['bowleronerun'] + \
     " Wickets: " + DATA['bowleronewickers'] + "\n\n" + "➡ Other Bowler: " + DATA['bowlertwo'] + " - Overs: " + \
@@ -21,7 +21,7 @@ querystring = {"title": "Cricket Score 🔴", "message": message}
 
 headers = {
     "Priority": "high",
-    "Icon": os.get('ICON')
+    "Icon": os.environ['ICON']
 }
 
 response = requests.request("POST", url, headers=headers, params=querystring)
